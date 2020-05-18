4047dcfd397d329def1ac2780638a14afc05c103
GhassanHariz <ghassan.hariz@outlook.com>
2020-05-18 19:17:46 +0200

    updated runserver.py

--- runserver.py
+++ runserver.py
@@ -3,5 +3,6 @@ from notejam.config import DevelopmentConfig
 
 app.config.from_object(DevelopmentConfig)
 
+# start of application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
